7f96660468b0a9000d13e7b0ae0fb935bb1770c9
Zarko Pavlovic <zarko@uboonegpvm01.fnal.gov>
2018-02-20 13:09:16 -0600

    Specify version of jobsub client and ifdhc.

--- scripts/setup_g4101.sh
+++ scripts/setup_g4101.sh
@@ -3,8 +3,8 @@ setup gcc v4_9_3
 setup geant4 v4_10_1_p03 -q e9:prof
 setup dk2nu v01_03_00b -q e9:prof:r5
 setup cmake v3_3_2
-setup jobsub_client
-setup ifdhc 
+setup jobsub_client v1_2_6
+setup ifdhc v2_1_0
 
 #use boost for beamHist.c
 setup boost v1_57_0a -q e9:prof
