d3f5d7e2d3f88a23ba5277ad8b20564b35142c5d
Marco Del Tutto <mdeltutt@fnal.gov>
2023-10-11 10:23:50 -0500

    Update setup scripts

--- scripts/setup_g4101.sh
+++ scripts/setup_g4101.sh
@@ -4,8 +4,8 @@ setup gcc v4_9_3
 setup geant4 v4_10_1_p03 -q e9:prof
 setup dk2nu v01_03_00b -q e9:prof:r5
 setup cmake v3_3_2
-setup jobsub_client v1_3_5
-setup ifdhc v2_1_0
+setup jobsub_client
+setup ifdhc
 
 #use boost for beamHist.c
 setup boost v1_57_0a -q e9:prof
--- scripts/setup_g4102.sh
+++ scripts/setup_g4102.sh
@@ -1,10 +1,11 @@
-source /grid/fermiapp/products/common/etc/setups
-source /grid/fermiapp/products/larsoft/setups
+source /cvmfs/larsoft.opensciencegrid.org/products/setups
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setup
+setup gcc v4_9_3
 setup geant4 v4_10_2_p02b -q e10:prof
 setup dk2nu v01_04_01f -q e10:prof:r6
 setup cmake v3_3_2
 setup jobsub_client
-setup ifdhc 
+setup ifdhc
 
 #use boost for beamHist.c
 setup boost v1_61_0 -q e10:prof
