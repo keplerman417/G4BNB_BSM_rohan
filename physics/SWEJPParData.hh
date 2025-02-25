c4dc1da61e67e40ffdf4e3850169d090d937174c
Rohan Rajagopalan <rohanr@sbndgpvm02.fnal.gov>
2025-02-25 16:11:15 -0600

    Added extended sanford wang parameter physics file

---
+++ physics/SWEJPParData.hh
@@ -0,0 +1,8 @@
+// File: SWParData.hh                                                         
+// --------------------------------------------------------------             
+// GEANT 4 - BooNEG4Beam Release: R1-12, 20-Nov-2005, D. Schmitz, M. Sorel, H. Tanaka, M. Wilking
+   // --------------------------------------------------------------             
+   //                                                                            
+   // Header description:                                                        
+   // This header file contains double-differential piplus and piminus           
+   // production cross-sections used by the BooNEpBeInteraction class
